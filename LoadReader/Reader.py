d136cc6ac7f1872113d6e2853581deeacc74010a
qisaw <wkas533@aucklanduni.ac.nz>
2014-10-06 17:04:27 +1300

    implemented retrieving of cpu information

--- LoadReader/Reader.py
+++ LoadReader/Reader.py
@@ -4,3 +4,13 @@ Created on 6/10/2014
 @author: wasiqkashkari
 '''
 import sys
+import psutil
+
+
+#cpu utilisation
+stats = psutil.cpu_percent(interval=1, percpu=True);
+print(stats)
+
+#cpu times
+stats_cup_times = psutil.cpu_times_percent(interval=1, percpu=True)
+print(stats_cup_times)
